53e8a2377f4a9bc089227dc5e293dd51b1d08039
h-kakeh <hasan.kakeh.dev@gmail.com>
2026-04-15 15:19:18 +0300

    update config

--- projects/ng-image-optimizer/server/config.ts
+++ projects/ng-image-optimizer/server/config.ts
@@ -60,7 +60,7 @@ export interface ImageConfig {
  * Can be overridden by passing custom options to the handler.
  */
 export const defaultConfig: ImageConfig = {
-  deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+  deviceSizes: [640, 750, 828, 1080, 1200, 1440, 1920, 2048],
   imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   remotePatterns: [],
   minimumCacheTTL: 14400,
